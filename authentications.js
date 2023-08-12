4e2be20e95c3ef32219053b8f8cd07954ff1e8db
Ashmeet670 <73339721+Ashmeet670@users.noreply.github.com>
2023-08-12 22:16:19 +0530

    Update authentications.js

--- authentications.js
+++ authentications.js
@@ -1,5 +1,31 @@
 //backedn auth ka code idhar
 
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-analytics.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAAuSmDXm3UbNcPTSNm5gUC1J5bmHoP0DU",
+    authDomain: "fire-ecology-f1ae7.firebaseapp.com",
+    projectId: "fire-ecology-f1ae7",
+    storageBucket: "fire-ecology-f1ae7.appspot.com",
+    messagingSenderId: "483478000209",
+    appId: "1:483478000209:web:e00d5eabe799d722e05e70",
+    measurementId: "G-N5VN01R8R8"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+
+
+
 function signIn() {
 
 }
