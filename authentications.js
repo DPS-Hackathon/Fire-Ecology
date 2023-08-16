bc9968370e1db10c4792d51a41c257e7186751e7
Aditya <82562840+AD0T-dev@users.noreply.github.com>
2023-08-16 18:50:59 +0530

    Made the signup
    Migrated to 8.2.7 from 10.1.0 version of firebase due to new API

--- authentications.js
+++ authentications.js
@@ -1,36 +1,44 @@
-//backedn auth ka code idhar
-
-
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-app.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-analytics.js";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
     apiKey: "AIzaSyAAuSmDXm3UbNcPTSNm5gUC1J5bmHoP0DU",
     authDomain: "fire-ecology-f1ae7.firebaseapp.com",
+    databaseURL: "https://fire-ecology-f1ae7-default-rtdb.asia-southeast1.firebasedatabase.app",
     projectId: "fire-ecology-f1ae7",
     storageBucket: "fire-ecology-f1ae7.appspot.com",
     messagingSenderId: "483478000209",
     appId: "1:483478000209:web:e00d5eabe799d722e05e70",
-    measurementId: "G-N5VN01R8R8"
-};
-
+  };
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+firebase.initializeApp(firebaseConfig);
 
+const auth = firebase.auth();
+const user = auth.currentUser;
 
-
-
-function signIn() {
-
-}
+const database = firebase.database();
 
 
 function signUp() {
 
+    let username_value = document.getElementById("signUpUsername");
+    let email = document.getElementById("signUpEmail");
+    let password = document.getElementById("signUpPassword");
+
+    auth.createUserWithEmailAndPassword(email.value, password.value)
+        .then(function (working) {
+            database.ref("users/"+auth.currentUser.uid).set(
+                {
+                    username:username_value.value
+                }
+            );
+            alert("Account created successfully");
+        })
+
+        .catch(function (error) {
+
+            let errorCode = error.code;
+            if (errorCode == "auth/weak-password") { alert("Weak Password","The password is too weak and the minimum size of password is 6 charecters"); }
+            else if (errorCode == "auth/email-already-in-use") { alert("Email alreasy used","The email is already in use"); email.value = ""; password.value = "";username_value.value = "" }
+            else if (errorCode == "auth/invalid-email") { alert("Invalid Email","The email id is invalid"); email.value = ""; password.value = "";username_value.value = ""}
+            else if (errorCode == "auth/operation-not-allowed") { alert("Email not","The email is not allowed"); email.value = ""; password.value = "";username_value.value = "" }
+            else { alert("Please try again",""); email.value = ""; password.value = "";username_value.value = ""}
+        });
 }
