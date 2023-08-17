99c1cacd1fec473278d28bc8b366fdaa1f254c53
Ashmeet670 <73339721+Ashmeet670@users.noreply.github.com>
2023-08-17 22:04:27 +0530

    removed comments

--- script.js
+++ script.js
@@ -13,12 +13,12 @@ firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
 const user = auth.currentUser;
 
-const database = firebase.database();
+const database = firebases.database();
 
 //Function to generate alerts 
-function generate_alert_error(title,description) {
+function generate_alert_error(title, description) {
     let document_area = document.getElementById("content-area");
-    document_area.insertAdjacentHTML("afterbegin",`<div class=\"alert alert-warning alert-dismissible fade show\" role=\"alert\"><strong>${title}</strong> ${description}<button type=\"button\" class=\"btn-close\" data-bs-dismiss=\"alert\" aria-label=\"Close\"></button></div>`);
+    document_area.insertAdjacentHTML("afterbegin", `<div class=\"alert alert-warning alert-dismissible fade show\" role=\"alert\"><strong>${title}</strong> ${description}<button type=\"button\" class=\"btn-close\" data-bs-dismiss=\"alert\" aria-label=\"Close\"></button></div>`);
 }
 
 function signUp() {
@@ -40,7 +40,7 @@ function signUp() {
         .catch(function (error) {
 
             let errorCode = error.code;
-            if (errorCode == "auth/weak-password") { generate_alert_error("Weak Password", "The password is too weak and the minimum size of password is 6 charecters");}
+            if (errorCode == "auth/weak-password") { generate_alert_error("Weak Password", "The password is too weak and the minimum size of password is 6 charecters"); }
             else if (errorCode == "auth/email-already-in-use") { generate_alert_error("Email alreasy used", "The email is already in use"); email.value = ""; password.value = ""; username_value.value = "" }
             else if (errorCode == "auth/invalid-email") { generate_alert_error("Invalid Email", "The email id is invalid"); email.value = ""; password.value = ""; username_value.value = "" }
             else if (errorCode == "auth/operation-not-allowed") { generate_alert_error("Email not", "The email is not allowed"); email.value = ""; password.value = ""; username_value.value = "" }
@@ -53,17 +53,17 @@ function signUp() {
     });
 }
 
-function signIn(){
+function signIn() {
     let email = document.getElementById("signInEmail");
     let password = document.getElementById("signInPassword");
     auth.signInWithEmailAndPassword(email.value, password.value).catch(function (error) {
 
         let errorCode = error.code;
 
-        if (errorCode == "auth/wrong-password") { generate_alert_error("Wrong Password","The password used for the email is incorrect"); email.value = ""; password.value = ""; }
-        else if (errorCode == "auth/invalid-email") { generate_alert_error("Invalid Email","The email is invalid"); email.value = ""; password.value = ""; }
-        else{
-        generate_alert_error("An error occured, please try again","");
+        if (errorCode == "auth/wrong-password") { generate_alert_error("Wrong Password", "The password used for the email is incorrect"); email.value = ""; password.value = ""; }
+        else if (errorCode == "auth/invalid-email") { generate_alert_error("Invalid Email", "The email is invalid"); email.value = ""; password.value = ""; }
+        else {
+            generate_alert_error("An error occured, please try again", "");
         }
         email.value = "";
         password.value = "";
@@ -88,8 +88,8 @@ function getDATA() {
                 postTitle = post.title;
                 postContent = post.content;
                 let document_area = document.getElementById("blogList")
-                document_area.insertAdjacentHTML("afterbegin",`<div class=\"col-8 col-sm-5 col-lg-4 col-xl-3 mx-1 blog-card rounded-2 shadow-lg my-4  \"><div class=\"text-center mb-4 mt-3 blog-img\"><img src=\"https://placehold.co/400x300\" class=\"img-fluid\" alt=\"\"></div><div class=\"text-center my-4 \"><h2 class=\"fs-4\">${post.title}</h2></div><div class=\"text-center mt-3 mb-5\"><h3 class=\"fs-6\">By: ${post.postAUthor}</h3></div><div class=\"text-center mt-4 mb-4\"><a href=\"\" class=\"btn buttonPost\">Read </a></div></div>`);
-                
+                document_area.insertAdjacentHTML("afterbegin", `<div class=\"col-8 col-sm-5 col-lg-4 col-xl-3 mx-1 blog-card rounded-2 shadow-lg my-4  \"><div class=\"text-center mb-4 mt-3 blog-img\"><img src=\"https://placehold.co/400x300\" class=\"img-fluid\" alt=\"\"></div><div class=\"text-center my-4 \"><h2 class=\"fs-4\">${post.title}</h2></div><div class=\"text-center mt-3 mb-5\"><h3 class=\"fs-6\">By: ${post.postAUthor}</h3></div><div class=\"text-center mt-4 mb-4\"><a href=\"\" class=\"btn buttonPost\">Read </a></div></div>`);
+
             });
             loadingIcon.classList.add('d-none')
         })
@@ -100,7 +100,7 @@ function getDATA() {
 
 let num = 0
 
-// function blog_save(){
+// function blog_save() {
 
 //     title_field = document.getElementById("title");
 //     author_field = document.getElementById("author");
@@ -109,9 +109,9 @@ let num = 0
 //     var userRef = database.ref('/blogs/');
 
 //     userRef.get().then(function then(snapshot) {
-//     num = snapshot.val();
-//     console.log(num);
-//     return num          
+//         num = snapshot.val();
+//         console.log(num);
+//         return num
 //     }
 //     let num = then()
 //     );
@@ -120,15 +120,13 @@ let num = 0
 // If user is signed in return him to homepage 
 firebase.auth().onAuthStateChanged((user) => {
     if (user) {
-        if (["signin.html#","signin.html#?","signin.html","signin.html?"].includes(location.href.split("/").slice(-1)[0]) || ["signup.html#","signup.html#?","signup.html","signup.html?"].includes(location.href.split("/").slice(-1)[0])) 
-        { 
-            window.location = "index.html"; 
+        if (["signin.html#", "signin.html#?", "signin.html", "signin.html?"].includes(location.href.split("/").slice(-1)[0]) || ["signup.html#", "signup.html#?", "signup.html", "signup.html?"].includes(location.href.split("/").slice(-1)[0])) {
+            window.location = "index.html";
         }
     }
-    else{
-        if (["blogwrite.html"].includes(location.href.split("/").slice(-1)[0])) 
-        { 
-            window.location = "signin.html"; 
+    else {
+        if (["blogwrite.html"].includes(location.href.split("/").slice(-1)[0])) {
+            window.location = "signin.html";
         }
     }
 });
